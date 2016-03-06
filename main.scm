4369bf4bfe576bb1a52a58d75c9cbf02f222c601
hiroki yagi <hiro.yagi0245@gmail.com>
2016-03-07 01:26:46 +0900

    character process version

--- main.scm
+++ main.scm
@@ -1,12 +1,22 @@
 (use srfi-69
-     (only miscmacros push! pop! inc! dec!)
+     (only miscmacros push! inc! dec!)
+     (only utils read-all)
+     (only srfi-1 reverse!)
      (prefix utf8 utf8:))
 ;; string-length, string->list, read-char, display
 (define *tape-length* 30000)
 (define *tape* (make-vector *tape-length* 0))
 (define *pointer* 0)
+(define (stop?)
+  (zero? (vector-ref *tape* *pointer*)))
 (define *while-stack* '())
 (define *while-count* 0)
+(define (initialize)
+  (set! *while-stack* '())
+  (set! *while-count* 0)
+  (set! *pointer 0)
+  (vector-fill! *tape* 0))
+;;; cとして渡されるのは char か *while-stack*
 (define (do-ops c)
   (case c
     [(#\+) (inc! (vector-ref *tape* *pointer*))]
@@ -15,65 +25,58 @@
     [(#\<) (dec! *pointer*)]
     [(#\,) (vector-set! *tape* *pointer* (char->integer (read-char)))]
     [(#\.) (display (integer->char (vector-ref *tape* *pointer*)))]
-    [else (for-each do-ops c)]))
-;;; state machine として
+    [else (when (list? c)
+            (do-while c))]))
 
-(define (divide lst x)
-  (let loop ([lst lst]
+(define (do-while w-stk)
+  (unless (or (stop?) (null? w-stk))
+    (for-each do-ops w-stk)
+    (do-while w-stk)))
+
+(define (process-char c)
+  (cond
+   [(zero? *while-count*)                 ; whileの外にいるとき
+    (case c
+      [(#\[)                            ; 最外のwhileスタート 
+       (inc! *while-count*)]            ; このときは push しない
+      [(#\])
+       (void)]
+      [(#\+ #\- #\> #\< #\, #\.)
+       (do-ops c)])]
+   [(stop?) ; whileしない
+    (set! *while-stack* '())
+    (case c
+      [(#\]) (dec! *while-count*)]
+      [(#\[) (inc! *while-count*)])]
+   [else                                ; whileの中にあって、(stop?)しない状態
+    (case c
+      [(#\[)
+       (inc! *while-count*)
+       (push! c *while-stack*)]
+      [(#\])
+       (dec! *while-count*)
+       (cond [(zero? *while-count*)   ; whileから抜けた?
+              (set! *while-stack* (reverse! *while-stack*))
+              (do-while *while-stack*)
+              (set! *while-stack* '())]
+             [(> *while-count* 0)
+              (set! *while-stack* (pack-while *while-stack*))]
+             [else (set! *while-stack* '())] ; while ] の 打ちすぎ(このエッジケースはあり得るか?)
+             )]
+      [(#\+ #\- #\> #\< #\, #\.)
+       (push! c *while-stack*)])]))
+
+;;; state machine として
+(define (pack-while while-stack)
+  (let loop ([lst while-stack]
              [acc '()])
     (cond [(null? lst) #f]
-          [(eq? (car lst) x) (values acc (cdr lst))]
+          [(eq? (car lst) #\[) (cons acc (cdr lst))]
           [else (loop (cdr lst) (cons (car lst) acc))])))
-
-(define (make-brainfucker tape-length)
-  (let ([tape (make-vector tape-length 0)]
-        [ptr   0]
-        [while-count 0]
-        [code '()])
-    (define (do-ops c)
-      (case c
-        [(#\+) (inc! (vector-ref tape ptr))]
-        [(#\-) (dec! (vector-ref tape ptr))]
-        [(#\>) (inc! ptr)]
-        [(#\<) (dec! ptr)]
-        [(#\,) (vector-set! tape ptr (char->integer (read-char)))]
-        [(#\.) (display (integer->char (vector-ref tape ptr)))]
-        [else (for-each do-ops c)]))
-    (define (process-char c)
-      (cond
-       [(zero? while-count)             ; whileの外にいるとき
-        (case c
-          [(#\+ #\- #\> #\< #\, #\.) (do-ops c)]
-          [(#\[)
-           (inc! while-count)
-           (push! c code)]
-          [(#\])
-           (void)]
-          [else (void)])]
-       [(zero? (vector-ref tape ptr))
-        (set! code '())
-        (case c
-          [(#\]) (dec! while-count)]
-          [(#\[) (inc! while-count)]
-          [else (void)])]
-       [else
-        (case c
-          [(#\+ #\- #\> #\< #\, #\.) (push! c (car code))]
-          [(#\[)
-           (inc! while-count)
-           (push! c code)]
-          [(#\])
-           (dec! while-count)
-           (if (zero? while-count)               ; whileから抜けた?
-               (if (zero? (vector-ref tape ptr)) ; 0であれば 
-                   (set! code '())               ; 終了
-                   (begin (set! code (reverse! code))
-                          (let loop ()
-                            (unless (zero? (vector-ref tape ptr))
-                              (do-ops code)
-                              (loop)))
-                          (set! code '())))
-               (receive (while-code rest) (divide code #\[)
-                 (set! code (cons while-code rest))))]
-          [else (void)])]))
-    process-char))
+(define (process-string str)
+  (for-each process-char (string->list str)))
+(define (bf-read-file file)
+  (initialize)
+  (process-string
+   (with-input-from-file file
+     read-all)))
